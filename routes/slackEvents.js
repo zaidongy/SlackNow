d03a7cc0c59fe4b5d329499598687c6c180453c2
Chris <zaidongy@ucla.edu>
2018-10-04 17:37:47 -0700

    cleaned up code and added link to rest call

--- routes/slackEvents.js
+++ routes/slackEvents.js
@@ -25,40 +25,13 @@ slackEvents.on('message', event => {
   _handleMessageOrMentionEvent2(event);
 });
 
-function _handleMessageOrMentionEvent(event) {
-  console.log(event);
-  if (!event.subtype) {
-    //event.subtype != 'bot_message' && event.subtype != 'channel_join'
-    //get the ticketNumber and lookup information on it
-    var ticket = snUtils.getTicketNumber(event.text);
-    if (ticket) {
-      snUtils.getTicketInfo(ticket, res => {
-        // console.log(res);
-        if (res)
-          web.chat.postMessage(
-            snUtils.buildMessage(event.channel, res, res.table))
-            .then(res => { console.log("Message sent: ", res.ts); })
-            .catch(console.error);
-      });
-
-    } else {
-      web.chat.postMessage({
-        channel: event.channel,
-        text: "Hey there, try giving me a ticket number."
-      })
-        .then(console.log("Unhandled query: ", event.text))
-        .catch(console.error);
-    }
-  }
-}
-
 function _handleMessageOrMentionEvent2(event) {
   console.log(event);
   if (!event.subtype) {
     // event.subtype != 'bot_message' && event.subtype != 'channel_join'
     // Chain Promises to execute sequence of events
     var number = snUtils.getTicketNumber(event.text);
-    
+
     snUtils.getTicketInfoPromise(number)
       .then(info => web.chat.postMessage(snUtils.buildMessage(event.channel, info, info.table)))
       .catch(() => {
